8476ff4245fc979ff8ca655f6863371cddfa5721
emanovic7 <e_owusu@live.com>
2019-06-07 00:06:17 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
 
 
+def my_collect(array)
+  final_array = []
+
+  if block_given?
+
+    i=0
+    while i < array.length
+      current_element = array[i]
+      final_array.push(yield current_element)
+
+      i += 1
+    end
+
+  else
+    "no block given"
+  end
+
+  final_array
+end
